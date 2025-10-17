45057b37e3e6efd3380e6ad58a83d75a7e228def
Umairabdullah2016 <ua8839705@gmail.com>
2025-10-17 18:39:32 +0100

    Fix import and update URL input prompt

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import streamlit as st
-import streamlit.components.v1 as component
+import streamlit.components.v1 as components  # use 'components', not 'component'
 
 st.title("Control A website!")
 
 # Create an entry box
-url = st.text_input("Enter url: ")
-
-
+url = st.text_input("Enter URL:")
 
 if st.button("Start Web"):
     iframe_code = f'<iframe src="{url}" width="800" height="600" frameborder="0"></iframe>'
-    component.html(iframe_code, height=600, scrolling=True)
+    components.html(iframe_code, height=600, scrolling=True)  # <- now correct
