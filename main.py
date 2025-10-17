5b4743c5a908260495209b588001378d171bf1d1
Umairabdullah2016 <ua8839705@gmail.com>
2025-10-17 18:40:35 +0100

    Change URL variable to url1 in iframe code

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import streamlit.components.v1 as components  # use 'components', not 'component
 st.title("Control A website!")
 
 # Create an entry box
-url = st.text_input("Enter URL:")
+url1 = st.text_input("Enter URL:")
 
 if st.button("Start Web"):
-    iframe_code = f'<iframe src="{url}" width="800" height="600" frameborder="0"></iframe>'
+    iframe_code = f'<iframe src="{url1}" width="800" height="600" frameborder="0"></iframe>'
     components.html(iframe_code, height=600, scrolling=True)  # <- now correct
